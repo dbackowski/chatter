77764e0614b627c0566f6673ec5bc3aa85819d49
Damian Baćkowski <dbackowski@artcom.pl>
2015-11-04 08:59:31 +0100

    fix bugs

--- public/app.js
+++ public/app.js
@@ -1,28 +1,10 @@
 $(document).ready(function() {
   var socket = io.connect("http://127.0.0.1:8080", { reconnection: true, transports: ['websocket', 'xhr-polling', 'polling', 'htmlfile', 'flashsocket'] });
 
-  socket.on('connect_error', function() {
-    $('#content').html('Wystąpił błąd, nie można pobrać danych.');
-  });
-
   socket.on('connect', function() {
     socket.emit('connected', { user: 'test' });
   });
-/*
-  socket.on('message', function(msg) {
-    console.log(msg);
-    $('#content').append($('<div>').html(msg['user']['login'] + ': ' + formatMessage(msg['body'])));
-  });
-*/
-/*
-  socket.on('history', function(msg) {
-    console.log(msg);
 
-    msg.forEach(function(msg) {
-      $('#content').append($('<div>').html(msg['user']['login'] + ': ' + formatMessage(msg['body'])));
-    })
-  });
-*/
   function formatMessage(msg) {
     return msg.replace(new RegExp('\ ','g'), '&nbsp;').replace(new RegExp('\r?\n','g'), '<br />');
   }
@@ -40,17 +22,10 @@ $(document).ready(function() {
     }
   }
 
-/*
-  $(document).on('keypress', '#input', function(e) {
-    if (e.keyCode === 13) {
-      socket.emit('message', { message: this.value, user_id: 1 });
-      this.value = '';
-    }
-  });
-*/
   var ChatApp = React.createClass({
     getInitialState: function() {
       socket.on('message', this.messageReceive);
+      socket.on('connect_error', this.connectionError);
       return { messages: [] };
     },
 
@@ -58,14 +33,18 @@ $(document).ready(function() {
       $.ajax({
         url: '/messages',
         dataType: 'json',
-        success: function(data) {
+        success: (data) => {
           console.log(data);
           this.setState({ messages: data });
-        }.bind(this),
-        failure: function(xhr, status, err) {
+        },
+        failure: (xhr, status, err) => {
           console.err(url, status, err.toString());
-        }.bind(this)
-    });
+        }
+      });
+    },
+
+    connectionError: function() {
+      $('#content').html('Wystąpił błąd, nie można pobrać danych.');
     },
 
     messageReceive: function(msg) {
