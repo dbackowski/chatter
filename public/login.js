756ab4dc162e9022152822823a42e976a183e782
Damian Baćkowski <dbackowski@artcom.pl>
2015-12-01 10:20:13 +0100

    fix bugs

--- public/login.js
+++ public/login.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
   "use strict";
 
+  var ErrorMessage = React.createClass({
+    render: function() {
+      return (
+        <div className="alert alert-danger" role="alert">
+          {this.props.message}
+        </div>
+      )
+    }
+  });
+
   var LoginForm = React.createClass({
     getInitialState: function() {
       return { login: "", password: "", error: "" }
+    },
+
+    updateLogin: function(e) {
+      this.setState({ login: e.target.value })
+    },
+
+    updatePassword: function(e) {
+      this.setState({ password: e.target.value })
     },
 
     onSubmit: function(e) {
@@ -26,15 +44,11 @@ $(document).ready(function() {
         type: 'POST',
         data: { login: login, password: password },
         success: (data) => {
-          //this.setState({data: data});
-          console.log('zalogowalem');
           if (data.error) {
-            console.log(data.error);
             this.setState({ error: data.error });
           } else {
             window.location.href = '/';
           }
-
         },
         error: (xhr, status, err) => {
           console.error(this.props.url, status, err.toString());
@@ -45,16 +59,17 @@ $(document).ready(function() {
     render: function() {
       return (
         <form onSubmit={this.onSubmit}>
-          {this.state.error}
+          { this.state.error ? <ErrorMessage message={this.state.error} /> : null }
+
           <div className="form-group">
             <label htmlFor="login">Login</label>
-            <input type="text" name="login" className="form-control" placeholder="Login" />
+            <input type="text" name="login" className="form-control" onChange={this.updateLogin} placeholder="Login" />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" className="form-control" placeholder="Password" />
+            <input type="password" name="password" className="form-control" onChange={this.updatePassword} placeholder="Password" />
           </div>
-          <button type="submit" className="btn btn-default">Login</button>
+          <button type="submit" className="btn btn-default" disabled={!this.state.login || !this.state.password}>Login</button>
         </form>
       )
     }
