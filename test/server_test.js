c017a6f6bf1432eb5a81c6f76745c3d3dea28717
Damian Baćkowski <dbackowski@artcom.pl>
2016-02-09 08:53:13 +0100

    fix bugs

--- test/server_test.js
+++ test/server_test.js
@@ -1,12 +1,12 @@
 process.env.NODE_ENV = 'test';
 
+var newXhr = require('socket.io-client-cookies-headers');
 var should = require('chai').should();
 var expect = require('chai').expect;
 var supertest = require('supertest');
 var api = supertest('http://localhost:8080');
 var app = require('../server.coffee');
 var fs = require('fs');
-var newXhr = require('socket.io-client-cookies-headers');
 var io = require('socket.io-client');
 var Cookies;
 
@@ -45,10 +45,15 @@ describe('Server', function() {
     describe('socket.io', function() {
       describe('on connection', function() {
         it('should throw error', function(done) {
-          var socket = io.connect('http://127.0.0.1:8080', { 'force new connection' : true });
+          var socket = io.connect('http://127.0.0.1:8080', {
+            'reconnection delay' : 0,
+            'reopen delay' : 0,
+            'force new connection' : true
+          });
 
           socket.on('error', function (data) {
             data.should.equal('Authentication error');
+            socket.disconnect();
             done();
           });
         });
@@ -81,10 +86,16 @@ describe('Server', function() {
       describe('on connection', function() {
         it('should emit connected user on users channel', function(done) {
           newXhr.setCookies(Cookies);
-          var socket = io.connect('http://127.0.0.1:8080', { 'force new connection' : true });
+
+          var socket = io.connect('http://127.0.0.1:8080', {
+            'reconnection delay' : 0,
+            'reopen delay' : 0,
+            'force new connection' : true
+          });
 
           socket.on('users', function (data) {
             data.should.deep.equal(new Array('test'));
+            socket.disconnect();
             done();
           });
         });
@@ -93,14 +104,20 @@ describe('Server', function() {
       describe('on send message', function() {
         it('should emit message on message channel', function(done) {
           newXhr.setCookies(Cookies);
-          var socket = io.connect('http://127.0.0.1:8080', { 'force new connection' : true });
 
-          socket.emit('message', { message: 'this is test message' });
+          var socket = io.connect('http://127.0.0.1:8080', {
+            'reconnection delay' : 0,
+            'reopen delay' : 0,
+            'force new connection' : true
+          });
 
           socket.on('message', function (data) {
             data.body.should.equal('this is test message');
+            socket.disconnect();
             done();
           });
+
+          socket.emit('message', { message: 'this is test message' });
         });
       });
     });
